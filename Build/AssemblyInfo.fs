de7e209502bbc7ac84022c485cdeececbac6b65c
Cameron Taggart <cameron.taggart@gmail.com>
2013-12-24 20:45:57 -0600

    adding AssemblyInfo.fs templates

---
+++ Build/AssemblyInfo.fs
@@ -0,0 +1,7 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyVersionAttribute("0.0.0")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.0")>]
+[<assembly: AssemblyInformationalVersionAttribute("0.0.0-a1312011730-aabbccdd")>]
+do ()
