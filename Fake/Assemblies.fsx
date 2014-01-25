33ae6f596c9e23b91e1919c3d4174c3a34811563
Cameron Taggart <cameron.taggart@gmail.com>
2014-01-24 20:25:58 -0600

    added #if MONO symbols, adjusted build scripts

--- Fake/Assemblies.fsx
+++ Fake/Assemblies.fsx
@@ -1,4 +1,22 @@
-﻿#r "Microsoft.TeamFoundation.Build.Client"
+﻿
+#r "System.Xml"
+#r "Microsoft.Build"
+#r "System.Configuration"
+
+#r "../packages/FAKE/tools/FakeLib.dll"
+#r "../packages/LibGit2Sharp.0.15.0.0/lib/net35/LibGit2Sharp.dll"
+#r "../packages/SourceLink.Fake/tools/SourceLink.dll"
+#r "../packages/SourceLink.Fake/tools/SourceLink.Git.dll"
+//#I __SOURCE_DIRECTORY__
+//#r "LibGit2Sharp.dll"
+//#r "SourceLink.dll"
+//#r "SourceLink.Git.dll"
+
+#if MONO
+#else
+#r "System.Net.Http"
+#r "System.Activities"
+#r "Microsoft.TeamFoundation.Build.Client"
 #r "Microsoft.TeamFoundation.Build.Workflow"
 #r "Microsoft.TeamFoundation.Client"
 #r "Microsoft.TeamFoundation.Common"
@@ -7,17 +25,7 @@
 #r "Microsoft.TeamFoundation.VersionControl.Common"
 #r "Microsoft.TeamFoundation.WorkItemTracking.Client"
 #r "Microsoft.VisualStudio.Services.Common"
-#r "System.Net.Http"
-#r "System.Xml"
-#r "Microsoft.Build"
-#r "System.Activities"
-#r "System.Configuration"
+#endif
 
-#I __SOURCE_DIRECTORY__
-#r "SourceLink.dll"
-#r "SourceLink.Tfs.dll"
-#r "LibGit2Sharp.dll"
-//#r "..\packages\SourceLink.0.3.0-a1401150837-89c6abbd\lib\Net45\SourceLink.dll" // when dev
-//#r "..\packages\LibGit2Sharp.0.15.0.0\lib\Net35\LibGit2Sharp.dll" // when dev
-//#r "..\packages\SourceLink.Tfs.0.3.0-a1401150837-89c6abbd\lib\Net45\SourceLink.Tfs.dll" // when dev
-//#r "..\packages\FAKE.2.4.8.0\Tools\FakeLib.dll" // when dev
+#r "../packages/SourceLink.Fake/tools/SourceLink.Tfs.dll" // in dev
+//#r "SourceLink.Tfs.dll"
---
+++ Fake/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+if [ ! -f packages/FAKE/tools/Fake.exe ]; then
+  mono --runtime=v4.0 .NuGet/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
+fi
+if [ ! -f packages/SourceLink.Fake/tools/Fake.fsx ]; then
+  mono --runtime=v4.0 .NuGet/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion
+fi
+mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
