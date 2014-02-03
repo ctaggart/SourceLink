a9c009b0495d48481e55d20e5f38260999f288fe
Cameron Taggart <cameron.taggart@gmail.com>
2014-02-03 13:11:10 -0600

    fix ctaggart/SourceLink#18 add location of TFS 2012 pdbstr.exe

--- SourceLink/Pdbstr.fs
+++ SourceLink/Pdbstr.fs
@@ -8,5 +8,6 @@ type Pdbstr() =
         @"C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x64\srcsrv\pdbstr.exe" // 6.3.9600.16384
         @"C:\Program Files\Microsoft Team Foundation Server 12.0\Tools\pdbstr.exe" // 6.3.9600.16384
         @"C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x64\srcsrv\pdbstr.exe" // 6.2.9200.16384
+        @"C:\Program Files\Microsoft Team Foundation Server 11.0\Tools\pdbstr.exe" // 6.2.9200.16384
         ]
         |> List.tryFind File.Exists
