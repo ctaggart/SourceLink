2e6c661a79f656f1a880889e8908b4ccf0d8c55f
Cameron Taggart <cameron.taggart@gmail.com>
2014-02-16 13:04:05 -0600

    added -d:MONO

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ fi
 if [ ! -f packages/SourceLink.Fake/tools/Fake.fsx ]; then
   mono --runtime=v4.0 .NuGet/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion -Prerelease
 fi
-mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx $@
+mono --runtime=v4.0 packages/FAKE/tools/FAKE.exe build.fsx -d:MONO $@
