51d01c574eec9d2d69f2ae00d3083b36abb47e1d
Cameron Taggart <cameron.taggart@gmail.com>
2014-04-08 22:01:05 -0500

    #20 troubleshooting NuGet.exe corruption

--- ConsoleTest/Program.fs
+++ ConsoleTest/Program.fs
@@ -6,8 +6,15 @@ open SourceLink
 //open SourceLink.PdbModify
 //open SourceLink.SrcSrv
 
+let getNugetExeShas() =
+    let f = @"C:\Projects\SourceLink\.nuget\NuGet.exe"
+    printfn "calculated: %s" (GitRepo.ComputeChecksum f)
+    use r = new GitRepo(@"C:\Projects\SourceLink")
+    printfn "in repo: %s" (r.Checksum f)
+
 [<EntryPoint>]
-let main argv = 
+let main argv =
+    getNugetExeShas()
 //    let mdd = Cor.CorMetaDataDispenser() :> Cor.IMetaDataDispenser
 //    printfn "mdd: %A" mdd
     0
--- Git/GitRepo.fs
+++ Git/GitRepo.fs
@@ -6,28 +6,8 @@ open LibGit2Sharp
 open System.Collections.Generic
 open System.Runtime.InteropServices
 
-///// Windows API
-//module Windows =
-//    [<DllImport("kernel32", CharSet=CharSet.Unicode)>]
-//    extern int AddDllDirectory(string dir)
-
-//type internal GitLib() =
-//    // similar to static initializer in LibGit2Sharp.Core.NativeMethods,
-//    // but it uses location of LibGit2Sharp.dll and AddDllDirectory instead of ExecutingAssembly and PATH
-//    static do
-//        try
-//            let dir = typeof<LibGit2SharpException>.Assembly.Location |> Path.GetDirectoryName
-//            let arch = if IntPtr.Size = 8 then "amd64" else "x86"
-//            let path = Path.combine dir (sprintf @"NativeBinaries\%s" arch)
-//            let i = path |> Windows.AddDllDirectory
-//            if i = 0 then
-//                Ex.failwithf "AddDllDirectory %A failed: %A" path (ComponentModel.Win32Exception(Marshal.GetLastWin32Error()))
-//        with
-//            | :? EntryPointNotFoundException -> Ex.failwithf "AddDllDirectory not found. Install KB2533623 update. http://support.microsoft.com/kb/2533623"
-
 type GitRepo(dir) =
     let dir = Path.absolute dir
-//    let gl = new GitLib() // trigger static initializer
     let repo = new Repository(dir)
     
     member x.Repo with get() = repo
@@ -43,6 +23,9 @@ type GitRepo(dir) =
         )
         |> Seq.toArray
 
+    static member ComputeChecksum file =
+        GitRepo.ComputeChecksums [file] |> Seq.head |> fst
+
     member x.Checksums files =
         files |> Seq.map (fun (file:string) ->
             let f =
@@ -54,6 +37,9 @@ type GitRepo(dir) =
             if ie <> null then file, ie.Id.Sha
             else file, ""
         )
+
+    member x.Checksum file =
+        x.Checksums [file] |> Seq.head |> snd
 
     member x.ChecksumSet files = 
         let checksums = 
