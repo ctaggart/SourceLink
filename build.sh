fcde558af4d8501eafc23887e1da34d8627455cd
Cameron Taggart <cameron@tachyus.com>
2014-11-26 19:48:03 -0800

    #50 default VisualStudioVersion to ProjectToolsVersion which is 12.0

--- build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mono .nuget/NuGet.exe install FAKE -OutputDirectory packages -ExcludeVersion
-mono .nuget/NuGet.exe install SourceLink.Fake -OutputDirectory packages -ExcludeVersion -Prerelease
-mono packages/FAKE/tools/FAKE.exe build.fsx -d:MONO $@
