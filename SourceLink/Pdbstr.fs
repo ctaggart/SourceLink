881d617512358ef9d77b8fe94dcc81cc30872e57
Cameron Taggart <cameron@tachyus.com>
2015-03-22 00:34:44 -0700

    have SourceLink.Fake look in chocolatey directory for pdbstr.exe

--- SourceLink/Pdbstr.fs
+++ SourceLink/Pdbstr.fs
@@ -1,10 +1,13 @@
 ﻿namespace SourceLink
 
+open System
 open System.IO
 
 type Pdbstr() =
     static member tryFind() =
         [
+        // Chocolatey, `cinst sourcelink` adds pdbstr too, default is C:\ProgramData\chocolatey\bin\pdbstr.exe
+        Path.combine (Environment.GetFolderPath Environment.SpecialFolder.CommonApplicationData) @"chocolatey\bin\pdbstr.exe"
         @"C:\Program Files (x86)\Windows Kits\8.1\Debuggers\x64\srcsrv\pdbstr.exe" // 6.3.9600.16384
         @"C:\Program Files\Microsoft Team Foundation Server 12.0\Tools\pdbstr.exe" // 6.3.9600.16384
         @"C:\Program Files (x86)\Windows Kits\8.0\Debuggers\x64\srcsrv\pdbstr.exe" // 6.2.9200.16384
