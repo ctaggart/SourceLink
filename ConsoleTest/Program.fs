cacbc253379de359745a5eef180f6468b3cb8ca6
Cameron Taggart <cameron@tachyus.com>
2015-04-05 19:11:37 -0700

    list indexed methods, troubleshooting #63

--- ConsoleTest/Program.fs
+++ ConsoleTest/Program.fs
@@ -6,34 +6,26 @@ open SourceLink
 open SourceLink.SymbolStore
 open System.Reflection
 
-let pdbSourceLink = @"..\..\..\packages\SourceLink.Fake\tools\SourceLink.pdb"
-let dllSourceLink = @"..\..\..\packages\SourceLink.Fake\tools\SourceLink.dll"
+//let pdbSourceLink = @"..\..\..\packages\SourceLink.Fake\tools\SourceLink.pdb"
+//let dllSourceLink = @"..\..\..\packages\SourceLink.Fake\tools\SourceLink.dll"
+let pdbSourceLink = @"..\..\..\packages\Octokit.0.8.0\lib\net45\Octokit.pdb"
+let dllSourceLink = @"..\..\..\packages\Octokit.0.8.0\lib\net45\Octokit.dll"
 let cachDir = @"..\..\..\packages"
-
-let printPdbDocuments() =
-    use pdb = File.OpenRead pdbSourceLink
-    let symbolCache = SymbolCache cachDir
-    let pdbReader = symbolCache.ReadPdb pdb pdbSourceLink
-    for d in pdbReader.Documents do
-        printfn "\npdb original source file path: %s" d.SourceFilePath
-        printfn "it had an md5 checksum of: %s" d.ChecksumHex
-        let url = pdbReader.GetDownloadUrl d.SourceFilePath |> Option.get
-        printfn "has download url if source indexed: %A" url
-        let downloadedFile = symbolCache.DownloadFile url
-        printfn "downloaded the file to the cache %s" downloadedFile
-        printfn "downloaded file has md5 of: %s" (Crypto.hashMD5 downloadedFile |> Hex.encode)
 
 let printMethods() =
     use pdb = File.OpenRead pdbSourceLink
     let symbolCache = SymbolCache cachDir
     let pdbReader = symbolCache.ReadPdb pdb pdbSourceLink
     let dll = Assembly.LoadFrom dllSourceLink
+    let md = dll.Modules |> Seq.head
+    printfn "module: %s" md.FullyQualifiedName
+    
     dll.DefinedTypes
 //    |> Seq.filter (fun dt -> dt.FullName = "SourceLink.VsBuild") // F# module
-    |> Seq.filter (fun dt -> dt.FullName = "SourceLink.PdbFile") // class
+//    |> Seq.filter (fun dt -> dt.FullName = "SourceLink.PdbFile") // class
     |> Seq.iter (fun dt ->
         for mbr in dt.GetMembers() do
-            printfn "%s, %s" dt.FullName mbr.Name
+            printfn "%s, %s, %d" dt.FullName mbr.Name mbr.MetadataToken
             match pdbReader.GetMethod mbr.MetadataToken with
             | None -> ()
             | Some mth ->
@@ -41,20 +33,31 @@ let printMethods() =
                     printfn "    %s %d %d" sp.Document.SourceFilePath sp.Line sp.Column
                     let downloadUrl = pdbReader.GetDownloadUrl sp.Document.SourceFilePath |> Option.get
                     printfn "    %s" downloadUrl
-                    let browserUrl = sprintf "%s#L%d" (downloadUrl.Replace("https://raw.githubusercontent.com/ctaggart/SourceLink", "https://github.com/ctaggart/SourceLink/blob")) sp.Line
+                    let browserUrl = sprintf "%s#L%d" (downloadUrl.Replace("https://raw.githubusercontent.com/octokit/octokit.net", "https://github.com/octokit/octokit.net/blob")) sp.Line
                     printfn "    %s" browserUrl 
     )
 
-let getItemsCompileLink() =
-    let p = VsProj.LoadRelease @"C:\Projects\FSharp.Compiler.Service\src\fsharp\FSharp.Compiler.Service\FSharp.Compiler.Service.fsproj"
-    for i in p.Items do
-        if i.HasMetadata "Link" then
-            printfn "%s" i.EvaluatedInclude
+/// gets all tokens in the pdb file
+let printTokens() =
+    use pdb = File.OpenRead pdbSourceLink
+    let symbolCache = SymbolCache cachDir
+    let pdbReader = symbolCache.ReadPdb pdb pdbSourceLink
+    let dll = Assembly.LoadFrom dllSourceLink
+    let md = dll.Modules |> Seq.head
+    printfn "module: %s" md.FullyQualifiedName
 
+    for d in pdbReader.Documents do
+        printfn "%s" d.SourceFilePath
+        for m in pdbReader.Reader.GetMethodsInDocument d do
+            let mth = md.ResolveMember m.Token
+            printfn "  %d %s %s" m.Token mth.DeclaringType.FullName mth.Name
+
+//    for m in pdbReader.Reader.Methods do
+//        let mth = md.ResolveMember m.Token
+//        printfn "%d %s %s" m.Token mth.DeclaringType.FullName mth.Name
 
 [<EntryPoint>]
 let main argv =
-//    printPdbDocuments()
-//    printMethods()
-    getItemsCompileLink()
+    printMethods()
+    printTokens()
     0
