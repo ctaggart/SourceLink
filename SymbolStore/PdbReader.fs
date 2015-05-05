ad7c3853905fb5af7252ef4bc9ba62a28c9125d6
Cameron Taggart <cameron@tachyus.com>
2015-05-04 21:48:32 -0700

    #73 better error message for invalid pdb

--- SymbolStore/PdbReader.fs
+++ SymbolStore/PdbReader.fs
@@ -5,14 +5,20 @@ open System.IO
 open SourceLink.SymbolStore.CorSym          
 
 type PdbReader(reader:ISymUnmanagedReader, sessionCookie:IntPtr, fileName:string) =
-
+    
     let moduleCookie =
         if String.IsNullOrEmpty fileName then 0L
         else SrcSrv.LoadModule(sessionCookie, fileName, reader.AsSourceServer)
     let isSourceIndexed = moduleCookie <> 0L
     
+    static let createReader pdb fileName = 
+        try
+            ISymUnmanagedReader.Create pdb
+        with
+            | _ -> failwithf "error reading pdb file '%s'" fileName
+
     new(pdb, sessionCookie, fileName) =
-        PdbReader(ISymUnmanagedReader.Create pdb, sessionCookie, fileName)
+        PdbReader(createReader pdb fileName, sessionCookie, fileName)
 
     new(pdb:Stream) =
         PdbReader(pdb, IntPtr.Zero, null)
