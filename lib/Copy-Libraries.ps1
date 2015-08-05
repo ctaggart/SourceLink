683df12afbe5d41cafcfc1c76efd2b2f48900fe7
Cameron Taggart <cameron@tachyus.com>
2015-08-05 00:24:30 -0700

    fix #101 switch to Microsoft.Build from Visual Studio 2015

---
+++ lib/Copy-Libraries.ps1
@@ -0,0 +1,3 @@
+copy 'C:\Program Files (x86)\MSBuild\14.0\Bin\Microsoft.Build.dll' $PSScriptRoot
+copy 'C:\Program Files (x86)\MSBuild\14.0\Bin\Microsoft.Build.Framework.dll' $PSScriptRoot
+copy 'C:\Program Files (x86)\MSBuild\14.0\Bin\Microsoft.Build.Utilities.Core.dll' $PSScriptRoot
