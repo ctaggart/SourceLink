b77c8cc1226ec1c85fb604ca4b745a7fa21534fd
Cameron Taggart <cameron.taggart@gmail.com>
2017-02-19 10:57:23 -0600

    dotnet sourcelink print-documents -p $pdb #133

--- build.ps1
+++ build.ps1
@@ -1,1 +1,40 @@
-.\packages\FAKE\tools\FAKE.exe build.fsx @args
+# the version under development, update after a release
+$version = '2.0.0'
+$versionSuffix = '-a031' # manually incremented for local builds
+
+function isVersionTag($tag){
+    $v = New-Object Version
+    [Version]::TryParse($tag, [ref]$v)
+}
+
+if ($env:appveyor){
+    $versionSuffix = '-b' + [int]::Parse($env:appveyor_build_number).ToString('000')
+    if ($env:appveyor_repo_tag -eq 'true' -and (isVersionTag($env:appveyor_repo_tag_name))){
+        $version = $env:appveyor_repo_tag_name
+        $versionSuffix = ''
+    }
+    Update-AppveyorBuild -Version "$version$versionSuffix"
+}
+
+$pack = "pack", "-c", "release", "--include-symbols", "-o", "../bin", "/p:Version=$version$versionSuffix"
+
+Push-Location .\dotnet-sourcelink
+dotnet restore
+dotnet $pack
+Pop-Location
+
+Push-Location .\dotnet-sourcelink-git
+dotnet restore
+dotnet $pack
+Pop-Location
+
+Push-Location .\SourceLink.Create.GitHub
+dotnet restore
+dotnet $pack
+Pop-Location
+
+# testing on local nuget feed
+if (-not $env:appveyor){
+    bash .\build-rename.sh
+    copy .\bin\*$version$versionSuffix.nupkg C:\dotnet\nupkg\
+}
--- dotnet-sourcelink/Program.cs
+++ dotnet-sourcelink/Program.cs
@@ -1,78 +1,138 @@
 ﻿using Microsoft.Extensions.CommandLineUtils;
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection.Metadata;
 using System.Text;
 
-class Program
-{
-    static int Main(string[] args)
+namespace SourceLink {
+    class Program
     {
-        var app = new CommandLineApplication()
+        static int Main(string[] args)
         {
-            Name = "dotnet sourcelink",
-            FullName = "SourceLink: Source Code On Demand",
-            Description = "Source Link your Portable PDB files to allow source code to be downloaded on demand from the source code repository host"
-        };
-        app.HelpOption("-h|--help");
-
-        app.Command("print-json", command =>
-        {
-            command.Description = "print the Source Link JSON stored in the Portable PDB file";
-            var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
-            command.HelpOption("-h|--help");
-
-            command.OnExecute(() =>
+            var app = new CommandLineApplication()
             {
-                if (!pdbOption.HasValue())
-                {
-                    command.ShowHelp();
-                    return 2;
-                }
-                var path = pdbOption.Value();
-                if (!File.Exists(path))
-                {
-                    Console.WriteLine("PDB file does not exist");
-                    return 3;
-                }
-                var bytes = GetSourceLinkBytes(path);
-                if(bytes.Length == 0)
-                {
-                    Console.WriteLine("Source Link JSON not found in PDB file");
-                    return 4;
-                }
-                Console.WriteLine(Encoding.UTF8.GetString(bytes));
-                return 0;
-            });
-        });
+                Name = "dotnet sourcelink",
+                FullName = "SourceLink: Source Code On Demand",
+                Description = "Source Link your Portable PDB files to allow source code to be downloaded on demand from the source code repository host"
+            };
+            app.HelpOption("-h|--help");
 
-        if (args.Length == 0)
-        {
-            app.ShowHelp();
+            app.Command("print-json", command =>
+            {
+                command.Description = "print the Source Link JSON stored in the Portable PDB file";
+                var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
+                command.HelpOption("-h|--help");
+
+                command.OnExecute(() =>
+                {
+                    if (!pdbOption.HasValue())
+                    {
+                        command.ShowHelp();
+                        return 2;
+                    }
+                    var path = pdbOption.Value();
+                    if (!File.Exists(path))
+                    {
+                        Console.WriteLine("PDB file does not exist");
+                        return 3;
+                    }
+
+                    var bytes = GetSourceLinkBytes(path);
+                    if (bytes.Length == 0)
+                    {
+                        Console.WriteLine("Source Link JSON not found in PDB file");
+                        return 4;
+                    }
+                    Console.WriteLine(Encoding.UTF8.GetString(bytes));
+
+                    return 0;
+                });
+            });
+
+            app.Command("print-documents", command =>
+            {
+                command.Description = "print the documents stored in the Portable PDB file";
+                var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
+                command.HelpOption("-h|--help");
+
+                command.OnExecute(() =>
+                {
+                    if (!pdbOption.HasValue())
+                    {
+                        command.ShowHelp();
+                        return 2;
+                    }
+                    var path = pdbOption.Value();
+                    if (!File.Exists(path))
+                    {
+                        Console.WriteLine("PDB file does not exist");
+                        return 3;
+                    }
+
+                    foreach(var doc in GetDocuments(path))
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                    }
+
+                    return 0;
+                });
+            });
+
+            if (args.Length == 0)
+            {
+                app.ShowHelp();
+                return 0;
+            }
+            app.Execute(args);
             return 0;
         }
-        app.Execute(args);
-        return 0;
-    }
 
-    static readonly Guid SourceLinkId = new Guid("CC110556-A091-4D38-9FEC-25AB9A351A6A");
+        static readonly Guid SourceLinkId = new Guid("CC110556-A091-4D38-9FEC-25AB9A351A6A");
 
-    static byte[] GetSourceLinkBytes(string pdb)
-    {
-        using (var mrp = MetadataReaderProvider.FromPortablePdbStream(File.OpenRead(pdb)))
+        static byte[] GetSourceLinkBytes(string pdb)
         {
-            var mr = mrp.GetMetadataReader();
-            var blobh = default(BlobHandle);
-            foreach (var cdih in mr.GetCustomDebugInformation(EntityHandle.ModuleDefinition))
+            using (var mrp = MetadataReaderProvider.FromPortablePdbStream(File.OpenRead(pdb)))
             {
-                var cdi = mr.GetCustomDebugInformation(cdih);
-                if (mr.GetGuid(cdi.Kind) == SourceLinkId)
+                var mr = mrp.GetMetadataReader();
+                var blobh = default(BlobHandle);
+                foreach (var cdih in mr.GetCustomDebugInformation(EntityHandle.ModuleDefinition))
                 {
-                    blobh = cdi.Value;
+                    var cdi = mr.GetCustomDebugInformation(cdih);
+                    if (mr.GetGuid(cdi.Kind) == SourceLinkId)
+                    {
+                        blobh = cdi.Value;
+                    }
                 }
+                if (blobh.IsNil) return Array.Empty<byte>();
+                return mr.GetBlobBytes(blobh);
             }
-            if (blobh.IsNil) return Array.Empty<byte>();
-            return mr.GetBlobBytes(blobh);
         }
+
+        static IEnumerable<Document> GetDocuments(string pdb)
+        {
+            using (var mrp = MetadataReaderProvider.FromPortablePdbStream(File.OpenRead(pdb)))
+            {
+                var mr = mrp.GetMetadataReader();
+                foreach (var dh in mr.Documents)
+                {
+                    var d = mr.GetDocument(dh);
+                    yield return new Document
+                    {
+                        Name = mr.GetString(d.Name),
+                        Language = mr.GetGuid(d.Language),
+                        HashAlgorithm = mr.GetGuid(d.HashAlgorithm),
+                        Hash = mr.GetBlobBytes(d.Hash),
+                    };
+                }
+
+            }
+        }
+
+        static string toHex(byte[] bytes)
+        {
+            return BitConverter.ToString(bytes).Replace("-", "").ToLowerInvariant();
+        }
+
     }
 }
