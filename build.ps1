28d0a30cf55fc977391c6c0a5c15f5d097d635ee
Cameron Taggart <cameron.taggart@gmail.com>
2017-02-20 09:12:06 -0600

    try Write-Output instead of echo

--- build.ps1
+++ build.ps1
@@ -1,6 +1,6 @@
 # the version under development, update after a release
 $version = '2.0.0'
-$versionSuffix = '-a040' # manually incremented for local builds
+$versionSuffix = '-a041' # manually incremented for local builds
 
 function isVersionTag($tag){
     $v = New-Object Version
@@ -35,7 +35,7 @@ Pop-Location
 
 # testing on local nuget feed
 if (-not $env:appveyor){
-    echo "not appveyor"
+    Write-Output "not appveyor"
     bash .\build-rename.sh
     copy .\bin\*$version$versionSuffix.nupkg C:\dotnet\nupkg\
 }
