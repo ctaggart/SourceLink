6c272cc66a257c81d33a87fa7d4bb3e0fe15c994
Cameron Taggart <cameron.taggart@gmail.com>
2017-02-19 23:02:15 -0600

    dotnet sourcelink test-urls path -p $pdb #135

--- dotnet-sourcelink/Program.cs
+++ dotnet-sourcelink/Program.cs
@@ -6,6 +6,10 @@ using System.IO;
 using System.Reflection.Metadata;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
 
 namespace SourceLink {
     class Program
@@ -20,111 +24,10 @@ namespace SourceLink {
             };
             app.HelpOption("-h|--help");
 
-            app.Command("print-json", command =>
-            {
-                command.Description = "print the Source Link JSON stored in the Portable PDB file";
-                var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
-                command.HelpOption("-h|--help");
-
-                command.OnExecute(() =>
-                {
-                    if (!pdbOption.HasValue())
-                    {
-                        command.ShowHelp();
-                        return 2;
-                    }
-                    var path = pdbOption.Value();
-                    if (!File.Exists(path))
-                    {
-                        Console.WriteLine("PDB file does not exist");
-                        return 3;
-                    }
-
-                    var bytes = GetSourceLinkBytes(path);
-                    if (bytes.Length == 0)
-                    {
-                        Console.WriteLine("Source Link JSON not found in PDB file");
-                        return 4;
-                    }
-                    Console.WriteLine(Encoding.UTF8.GetString(bytes));
-
-                    return 0;
-                });
-            });
-
-            app.Command("print-documents", command =>
-            {
-                command.Description = "print the documents stored in the Portable PDB file";
-                var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
-                command.HelpOption("-h|--help");
-
-                command.OnExecute(() =>
-                {
-                    if (!pdbOption.HasValue())
-                    {
-                        command.ShowHelp();
-                        return 2;
-                    }
-                    var path = pdbOption.Value();
-                    if (!File.Exists(path))
-                    {
-                        Console.WriteLine("PDB file does not exist");
-                        return 3;
-                    }
-
-                    foreach(var doc in GetDocuments(path))
-                    {
-                        Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
-                    }
-
-                    return 0;
-                });
-            });
-
-            app.Command("print-urls", command =>
-            {
-                command.Description = "print the urls for each document too based on the source link json";
-                var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
-                command.HelpOption("-h|--help");
-
-                command.OnExecute(() =>
-                {
-                    if (!pdbOption.HasValue())
-                    {
-                        command.ShowHelp();
-                        return 2;
-                    }
-                    var path = pdbOption.Value();
-                    if (!File.Exists(path))
-                    {
-                        Console.WriteLine("PDB file does not exist");
-                        return 3;
-                    }
-
-                    var missing = new List<Document>();
-                    foreach (var doc in GetDocumentsWithUrls(path))
-                    {
-                        if(doc.Url != null)
-                        {
-                            Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
-                            Console.WriteLine(doc.Url);
-                        } else {
-                            missing.Add(doc);
-                        }
-                    }
-                    if(missing.Count > 0)
-                    {
-                        Console.WriteLine("" + missing.Count + " Documents with URLs:");
-                        foreach(var doc in missing)
-                        {
-                            Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
-                        }
-                        return 4;
-                    }
-
-                    return 0;
-                });
-            });
+            app.Command("print-json", PrintJson);
+            app.Command("print-documents", PrintDocuments);
+            app.Command("print-urls", PrintUrls);
+            app.Command("test-urls", TestUrls);
 
             if (args.Length == 0)
             {
@@ -133,6 +36,174 @@ namespace SourceLink {
             }
             app.Execute(args);
             return 0;
+        }
+
+        static void PrintJson(CommandLineApplication command)
+        {
+            command.Description = "print the Source Link JSON stored in the Portable PDB file";
+            var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
+            command.HelpOption("-h|--help");
+
+            command.OnExecute(() =>
+            {
+                if (!pdbOption.HasValue())
+                {
+                    command.ShowHelp();
+                    return 2;
+                }
+                var path = pdbOption.Value();
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("PDB file does not exist");
+                    return 3;
+                }
+
+                var bytes = GetSourceLinkBytes(path);
+                if (bytes.Length == 0)
+                {
+                    Console.WriteLine("Source Link JSON not found in PDB file");
+                    return 4;
+                }
+                Console.WriteLine(Encoding.UTF8.GetString(bytes));
+
+                return 0;
+            });
+        }
+
+        static void PrintDocuments(CommandLineApplication command)
+        {
+            command.Description = "print the documents stored in the Portable PDB file";
+            var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
+            command.HelpOption("-h|--help");
+
+            command.OnExecute(() =>
+            {
+                if (!pdbOption.HasValue())
+                {
+                    command.ShowHelp();
+                    return 2;
+                }
+                var path = pdbOption.Value();
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("PDB file does not exist");
+                    return 3;
+                }
+
+                foreach (var doc in GetDocuments(path))
+                {
+                    Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                }
+
+                return 0;
+            });
+        }
+
+        static void PrintUrls(CommandLineApplication command)
+        {
+            command.Description = "print the urls for each document too based on the source link json";
+            var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
+            command.HelpOption("-h|--help");
+
+            command.OnExecute(() =>
+            {
+                if (!pdbOption.HasValue())
+                {
+                    command.ShowHelp();
+                    return 2;
+                }
+                var path = pdbOption.Value();
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("PDB file does not exist");
+                    return 3;
+                }
+
+                var missingDocs = new List<Document>();
+                foreach (var doc in GetDocumentsWithUrls(path))
+                {
+                    if (doc.Url != null)
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                        Console.WriteLine(doc.Url);
+                    } else {
+                        missingDocs.Add(doc);
+                    }
+                }
+                if (missingDocs.Count > 0)
+                {
+                    Console.WriteLine("" + missingDocs.Count + " Documents without URLs:");
+                    foreach (var doc in missingDocs)
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                    }
+                    return 4;
+                }
+
+                return 0;
+            });
+        }
+
+        static void TestUrls(CommandLineApplication command)
+        {
+            command.Description = "print the urls for each document too based on the source link json";
+            var pdbOption = command.Option("-p|--pdb <PDB>", "set path to Porable PDB", CommandOptionType.SingleValue);
+            command.HelpOption("-h|--help");
+
+            command.OnExecute(() =>
+            {
+                if (!pdbOption.HasValue())
+                {
+                    command.ShowHelp();
+                    return 2;
+                }
+                var path = pdbOption.Value();
+                if (!File.Exists(path))
+                {
+                    Console.WriteLine("PDB file does not exist");
+                    return 3;
+                }
+
+                var missingDocs = new List<Document>();
+                var erroredDocs = new List<Document>();
+                foreach (var doc in GetDocumentsWithUrlHashes(path))
+                {
+                    if (doc.Url != null)
+                    {
+                        if(doc.Error == null)
+                        {
+                            Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                            Console.WriteLine(doc.Url);
+                        } else {
+                            erroredDocs.Add(doc);
+                        }
+                    } else {
+                        missingDocs.Add(doc);
+                    }
+                }
+                if (missingDocs.Count > 0)
+                {
+                    Console.WriteLine("" + missingDocs.Count + " Documents without URLs:");
+                    foreach (var doc in missingDocs)
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                    }
+                }
+                if (erroredDocs.Count > 0)
+                {
+                    Console.WriteLine("" + erroredDocs.Count + " Documents with errors:");
+                    foreach (var doc in erroredDocs)
+                    {
+                        Console.WriteLine("{0} {1} {2} {3}", toHex(doc.Hash), HashAlgorithmGuids.GetName(doc.HashAlgorithm), LanguageGuids.GetName(doc.Language), doc.Name);
+                        Console.WriteLine(doc.Url);
+                        Console.WriteLine("error: " + doc.Error);
+                    }
+                }
+                if (missingDocs.Count > 0 || erroredDocs.Count > 0)
+                    return 4;
+
+                return 0;
+            });
         }
 
         static readonly Guid SourceLinkId = new Guid("CC110556-A091-4D38-9FEC-25AB9A351A6A");
@@ -205,14 +276,76 @@ namespace SourceLink {
                     var url = json.documents[key];
                     var path = m.Groups[1].Value.Replace(@"\", "/");
                     return url.Replace("*", path);
-                }
-                else
-                {
+                } else {
                     if (!key.Equals(file, StringComparison.Ordinal)) continue;
                     return json.documents[key];
                 }
             }
             return null;
+        }
+
+        static IEnumerable<Document> GetDocumentsWithUrlHashes(string pdb)
+        {
+            // https://github.com/ctaggart/SourceLink/blob/v1/Exe/Http.fs
+            // https://github.com/ctaggart/SourceLink/blob/v1/Exe/Checksums.fs
+
+            var handler = new HttpClientHandler();
+            if (handler.SupportsAutomaticDecompression)
+                handler.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            using (var hc = new HttpClient(handler))
+            {
+                hc.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("SourceLink", "2.0.0"));
+                // TODO Basic Auth support, ASCII or UTF8
+                //hc.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue("Basic",
+                //    Convert.ToBase64String(Encoding.ASCII.GetBytes("username:password")));
+
+                foreach (var doc in GetDocumentsWithUrls(pdb))
+                {
+                    if(doc.Url != null)
+                    {
+                        HashUrl(hc, doc);
+                        if (doc.Error != null) continue;
+                        if (!doc.Hash.CollectionEquals(doc.UrlHash))
+                        {
+                            doc.Error = "url hash does not match: " + toHex(doc.Hash);
+                        }
+                    }
+                    yield return doc;
+                }
+            }
+        }
+
+        // TODO async
+        static void HashUrl(HttpClient hc, Document doc)
+        {
+            using (var req = new HttpRequestMessage(HttpMethod.Get, doc.Url))
+            {
+                using (var rsp = hc.SendAsync(req).Result)
+                {
+                    if (rsp.IsSuccessStatusCode)
+                    {
+                        using (var stream = rsp.Content.ReadAsStreamAsync().Result)
+                        {
+                            // TODO Is it more efficient to cache?
+                            using(var ha = CreateHashAlgorithm(doc.HashAlgorithm))
+                            {
+                                doc.UrlHash = ha.ComputeHash(stream);
+                            }
+                        }
+                    } else {
+                        doc.Error = "url failed " + rsp.StatusCode + ": " + rsp.ReasonPhrase;
+                    }
+                }
+            }       
+        }
+
+        // IDisposable
+        public static HashAlgorithm CreateHashAlgorithm(Guid guid)
+        {
+            if (guid == HashAlgorithmGuids.md5) return MD5.Create();
+            if (guid == HashAlgorithmGuids.sha1) return SHA1.Create();
+            if (guid == HashAlgorithmGuids.sha256) return SHA256.Create();
+            throw new CryptographicException("unknown HashAlgorithm " + guid);
         }
 
     }
---
+++ dotnet-sourcelink/SystemExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SourceLink
+{
+    // https://github.com/ctaggart/SourceLink/blob/v1/SourceLink/SystemExtensions.fs
+    public static class SystemExtensions
+    {
+        public static bool CollectionEquals<T>(this ICollection<T> a, ICollection<T> b)
+        {
+            if (a.Count != b.Count)
+                return false;
+            return a.SequenceEqual(b);
+        }
+    }
+}
