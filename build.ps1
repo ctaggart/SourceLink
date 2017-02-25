e197314bc4c7a69807e356dd4e17397ab743a968
Cameron Taggart <cameron.taggart@gmail.com>
2017-02-25 16:43:24 -0600

    include explicit dll reference

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 $version = '2.0.0' # the version under development, update after a release
-$versionSuffix = '-a080' # manually incremented for local builds
+$versionSuffix = '-a081' # manually incremented for local builds
 
 function isVersionTag($tag){
     $v = New-Object Version
