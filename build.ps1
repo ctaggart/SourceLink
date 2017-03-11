78d969d36456dbecb694bee5a1880486bd6e77de
Cameron Taggart <cameron.taggart@gmail.com>
2017-03-10 22:09:11 -0600

    this will be 2.0.2

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-$version = '2.1.0' # the version under development, update after a release
+$version = '2.0.2' # the version under development, update after a release
 $versionSuffix = '-a115' # manually incremented for local builds
 
 function isVersionTag($tag){
