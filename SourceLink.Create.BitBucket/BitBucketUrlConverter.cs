9f1f1daac2928a4246ada22915e4ec0235310985
Chris Stylianou <chris5287@gmail.com>
2017-09-05 16:42:09 +0100

    Added Bitbucket Server regex for urls.

--- SourceLink.Create.BitBucket/BitBucketUrlConverter.cs
+++ SourceLink.Create.BitBucket/BitBucketUrlConverter.cs
@@ -1,11 +1,14 @@
 ﻿
+using System.Text.RegularExpressions;
+
 namespace SourceLink.Create.BitBucket
 {
     public static class UrlConverter
     {
         public static string Convert(string origin)
         {
-            if (!origin.Contains("bitbucket.org")) return null;
+            // Process as Bitbucket Server url if not from bitbucket.org
+            if (!origin.Contains("bitbucket.org")) return ConvertServer(origin);
             if (origin.StartsWith("git@"))
             {
                 origin = origin.Replace(':', '/');
@@ -14,6 +17,27 @@ namespace SourceLink.Create.BitBucket
             origin = origin.Replace(".git", "");
             var uri = new System.Uri(origin);
             return "https://bitbucket.org" + uri.LocalPath + "/raw/{commit}/*";
+        }
+
+        public static string ConvertServer(string origin)
+        {
+            // Try match https:// url.
+            var match = Regex.Match(origin, @"^https:\/\/(.*)\/scm\/(.*)\/(.*)\.git$", RegexOptions.IgnoreCase);
+            if (match.Success == false)
+            {
+                // Try match ssh://git url.
+                match = Regex.Match(origin, @"^ssh:\/\/git@(.*):\d+\/(.*)\/(.*)\.git$", RegexOptions.IgnoreCase);
+            }
+
+            // Any match?
+            if (match.Success)
+            {
+                return $"https://{match.Groups[1]}/projects/{match.Groups[2]}/repos/{match.Groups[3]}/raw/*?at={{commit}}";
+            }
+            else
+            {
+                return null;
+            }
         }
     }
 }
