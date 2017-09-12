2d27ca532daad579a6f145626722f66c462af8d6
Sean Feldman <feldman.sean@gmail.com>
2017-09-12 13:00:51 -0600

    fixing implementations

--- SourceLink.Create.GitHub/GitHubUrlConverter.cs
+++ SourceLink.Create.GitHub/GitHubUrlConverter.cs
@@ -5,15 +5,19 @@ namespace SourceLink.Create.GitHub
     {
         public static string Convert(string origin)
         {
-            if (!origin.Contains("github.com")) return null;
+            if (!origin.Contains("github.com"))
+                return null;
+
             if (origin.StartsWith("git@"))
             {
                 origin = origin.Replace(':', '/');
                 origin = origin.Replace("git@", "https://");
             }
+
             origin = origin.Replace(".git", string.Empty);
-            origin = origin.Replace("/", string.Empty);
+            origin = origin.TrimEnd('/');
             var uri = new System.Uri(origin);
+
             return $"https://raw.githubusercontent.com{uri.LocalPath}/{{commit}}/*";
         }
     }
