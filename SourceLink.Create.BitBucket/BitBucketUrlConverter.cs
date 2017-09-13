c9d660371c07c729719d23224bf1896c3d1a54b3
Sean Feldman <SeanFeldman@users.noreply.github.com>
2017-09-13 11:34:05 -0600

    URLs with trailing slash (#240)
    * fixes #232
    
    * additional test case
    
    * Updated BitBucket URL converter as well
    
    * merged rebase conflict
    
    * fixing implementations
    
    * Today's special: test one one cover another for the same price

--- SourceLink.Create.BitBucket/BitBucketUrlConverter.cs
+++ SourceLink.Create.BitBucket/BitBucketUrlConverter.cs
@@ -1,19 +1,22 @@
-﻿
-namespace SourceLink.Create.BitBucket
+﻿namespace SourceLink.Create.BitBucket
 {
     public static class UrlConverter
     {
         public static string Convert(string origin)
         {
-            if (!origin.Contains("bitbucket.org")) return null;
+            if (!origin.Contains("bitbucket.org"))
+                return null;
+
             if (origin.StartsWith("git@"))
             {
                 origin = origin.Replace(':', '/');
                 origin = origin.Replace("git@", "https://");
             }
-            origin = origin.Replace(".git", "");
+            origin = origin.Replace(".git", string.Empty);
+            origin = origin.TrimEnd('/');
+
             var uri = new System.Uri(origin);
-            return "https://bitbucket.org" + uri.LocalPath + "/raw/{commit}/*";
+            return $"https://bitbucket.org{uri.LocalPath}/raw/{{commit}}/*";
         }
     }
 }
--- SourceLink.Create.GitHub/GitHubUrlConverter.cs
+++ SourceLink.Create.GitHub/GitHubUrlConverter.cs
@@ -1,19 +1,23 @@
-﻿
-namespace SourceLink.Create.GitHub
+﻿namespace SourceLink.Create.GitHub
 {
     public static class UrlConverter
     {
         public static string Convert(string origin)
         {
-            if (!origin.Contains("github.com")) return null;
+            if (!origin.Contains("github.com"))
+                return null;
+
             if (origin.StartsWith("git@"))
             {
                 origin = origin.Replace(':', '/');
                 origin = origin.Replace("git@", "https://");
             }
-            origin = origin.Replace(".git", "");
+
+            origin = origin.Replace(".git", string.Empty);
+            origin = origin.TrimEnd('/');
             var uri = new System.Uri(origin);
-            return "https://raw.githubusercontent.com" + uri.LocalPath + "/{commit}/*";
+
+            return $"https://raw.githubusercontent.com{uri.LocalPath}/{{commit}}/*";
         }
     }
 }
