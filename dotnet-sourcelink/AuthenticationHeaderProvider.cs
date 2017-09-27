062989dbb9779bbd784d7056677fca0d3f526d93
Jonathan Kuleff <jonathankuleff@gmail.com>
2017-09-28 01:11:03 +1000

    support basic authentication (#266)

---
+++ dotnet-sourcelink/AuthenticationHeaderProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace SourceLink
+{
+    public interface IAuthenticationHeaderValueProvider
+    {
+        AuthenticationHeaderValue GetValue();
+    }
+
+    internal class BasicAuthenticationHeaderValueProvider : IAuthenticationHeaderValueProvider
+    {
+        private readonly string _username;
+        private readonly string _password;
+        private readonly Encoding _encoding;
+
+        public BasicAuthenticationHeaderValueProvider(string username, string password, Encoding encoding = null)
+        {
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentNullException(nameof(username),"Invalid username value");
+            if (string.IsNullOrWhiteSpace(password)) throw new ArgumentNullException(nameof(password),"Invalid password value");
+
+            _username = username;
+            _password = password;
+            _encoding = encoding ?? Encoding.ASCII;
+        }
+
+        public AuthenticationHeaderValue GetValue()
+        {
+            return new AuthenticationHeaderValue("Basic", Convert.ToBase64String(_encoding.GetBytes($"{_username}:{_password}")));
+        }
+    }
+}
