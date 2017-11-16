2bf780650b9d62b08cdc44b8d2532b19c8c8dcf5
jnm2 <me@jnm2.com>
2017-11-15 19:14:34 -0500

    Set up integration tests on targets packages

---
+++ Tests/Helpers/TempDirectory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace Tests.Helpers
+{
+    [DebuggerDisplay("{ToString(),nq}")]
+    public sealed class TempDirectory : IDisposable
+    {
+        public TempDirectory() : this(System.IO.Path.Combine(System.IO.Path.GetTempPath(), System.IO.Path.GetRandomFileName()))
+        {
+        }
+
+        public TempDirectory(string path)
+        {
+            Directory.CreateDirectory(path);
+            this.path = path;
+        }
+
+        private string path;
+        public string Path => path;
+
+        public static implicit operator string(TempDirectory tempFile) => tempFile.path;
+
+        public override string ToString() => path;
+
+        public void Dispose()
+        {
+            var path = Interlocked.Exchange(ref this.path, null);
+            if (path != null) Directory.Delete(path, recursive: true);
+        }
+    }
+}
---
+++ Tests/Integration/When_package_without_lib_is_installed.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using Tests.Helpers;
+using Xunit;
+
+namespace Tests.Integration
+{
+    public static class When_package_without_lib_is_installed
+    {
+        [Theory]
+        [InlineData("SourceLink.Create.BitBucket")]
+        [InlineData("SourceLink.Create.BitBucketServer")]
+        [InlineData("SourceLink.Create.CommandLine")]
+        [InlineData("SourceLink.Create.GitHub")]
+        [InlineData("SourceLink.Create.GitLab")]
+        [InlineData("SourceLink.Embed.AllSourceFiles")]
+        [InlineData("SourceLink.Embed.PaketFiles")]
+        [InlineData("SourceLink.Test")]
+        public static void Should_not_reference_additional_libraries(string packageName)
+        {
+            var packageSource = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Integration");
+
+            var packageFile = Assert.Single(Directory.GetFiles(packageSource, $"{packageName}.*.nupkg"));
+            var packageVersion = Path.GetFileNameWithoutExtension(packageFile).Substring(packageName.Length + 1);
+
+            const string targetFramework = "net462";
+            using (var fixture = new CsprojFixture(new[]
+            {
+                "<Project Sdk=\"Microsoft.Net.Sdk\">",
+                "  <PropertyGroup>",
+               $"    <TargetFramework>{targetFramework}</TargetFramework>",
+                "  </PropertyGroup>",
+                "  <ItemGroup>",
+               $"    <PackageReference Include=\"{packageName}\" Version=\"{packageVersion}\" PrivateAssets=\"all\" />",
+                "  </ItemGroup>",
+                "</Project>"
+            }))
+            {
+                fixture.AddFile("test.cs", Array.Empty<string>());
+
+                fixture.DotnetRestore(packageSource);
+                fixture.DotnetMSBuild();
+
+                Assert.Equal(
+                    new[]
+                    {
+                        $@"bin\Debug\{targetFramework}\test.dll",
+                        $@"bin\Debug\{targetFramework}\test.pdb"
+                    },
+                    fixture.GetFiles($@"bin\Debug\{targetFramework}", SearchOption.AllDirectories));
+            }
+        }
+    }
+}
